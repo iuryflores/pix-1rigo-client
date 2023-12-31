e7cd84df5a9cd5f08e1bc32dec39f4fa4a2ae7dd
Iury H. Flores <iury.flores@gmail.com>
2023-12-31 10:35:42 -0300

    set routes and install qrcode.react

---
+++ src/utils/api.utils.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+class Api {
+  constructor() {
+    this.api = axios.create({
+      baseURL: "http://localhost:9002",
+    });
+  }
+  consultaCobranca = async () => {
+    try {
+      const { data } = await this.api.get("/v1/consultar-todos-pix/");
+      return data;
+    } catch (error) {
+      throw error.response.data.msg;
+    }
+  };
+  gerarCobranca = async () => {
+    try {
+      const { data } = await this.api.put("/v1/gerar-pix/");
+      return data;
+    } catch (error) {
+      throw error.response.data.msg;
+    }
+  };
+}
+export default new Api();
